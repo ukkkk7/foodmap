1c292bbc97eec3633a3d7e087227b684273bc0f8
cyyoun <cyyoun41@gmail.com>
2023-12-14 11:39:36 +0900

    Feat: Grade Enum 타입으로 변경

--- src/main/java/pairproject/foodmap/domain/Grade.java
+++ src/main/java/pairproject/foodmap/domain/Grade.java
@@ -3,9 +3,16 @@ package pairproject.foodmap.domain;
 import lombok.Getter;
 
 @Getter
-public class Grade {
-    private long gradeId;
-    private String name;
-    private int rangeMin;
-    private int rangeMax;
+public enum Grade {
+    LEV1("맛린이"),
+    LEV2("맛초보"),
+    LEV3("맛중수"),
+    LEV4("맛고수"),
+    LEV5("맛잘알");
+
+    private final String name;
+    
+    Grade(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/pairproject/foodmap/service/GradeService.java
+++ src/main/java/pairproject/foodmap/service/GradeService.java
@@ -1,28 +1,36 @@
 package pairproject.foodmap.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pairproject.foodmap.domain.Grade;
-import pairproject.foodmap.repository.GradeMapper;
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class GradeService {
 
-    private final GradeMapper gradeMapper;
     private final UserService userService;
-    public void updateGrade(int followerCount, long userId) {
-        int gradeId = gradeMapper.findGradeIdByRange(followerCount);
-        userService.updateGrade(gradeId, userId);
-        System.out.println(userId + "님, 현재 등급은 " + gradeId + "입니다.");
-    }
 
-    public void createGrade(Grade grade) {
-        gradeMapper.save(grade);
-    }
-    public void deleteGrade(long gradeId) {
-        gradeMapper.deleteById(gradeId);
+    public void updateGrade(int followerCount, long userId) {
+        Grade grade;
+
+        if (followerCount < 100) {
+            return;
+        } else if (followerCount < 300) {
+            grade = Grade.LEV2;
+        } else if (followerCount < 500) {
+            grade = Grade.LEV3;
+        } else if (followerCount < 1000) {
+            grade = Grade.LEV4;
+        } else {
+            grade = Grade.LEV5;
+        }
+
+        userService.updateUserGrade(grade, userId);
+        log.trace("{} 님의 등급이 {} 으로 업데이트 되었습니다. ", userId, grade.getName());
     }
 }
+
