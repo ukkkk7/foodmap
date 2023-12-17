c57a501c648b79b305922630e25a0cc6a30dbbd2
cyyoun <cyyoun41@gmail.com>
2023-12-17 20:57:28 +0900

    Feat: 예외처리 추가 누락본

---
+++ src/main/java/pairproject/foodmap/exception/FileOperationException.java
@@ -0,0 +1,7 @@
+package pairproject.foodmap.exception;
+
+public class FileOperationException extends RuntimeException {
+    public FileOperationException(String message) {
+        super(message);
+    }
+}
