c050b287a83b122c12428a693026eda896f0a4df
cyyoun <cyyoun41@gmail.com>
2023-12-19 19:46:20 +0900

    Test: Service - Category 단위테스트

--- src/main/java/pairproject/foodmap/service/CategoryService.java
+++ src/main/java/pairproject/foodmap/service/CategoryService.java
@@ -1,12 +1,13 @@
 package pairproject.foodmap.service;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pairproject.foodmap.domain.Category;
+import pairproject.foodmap.exception.DuplicateUserException;
+import pairproject.foodmap.exception.exHandler.JsonParsingException;
 import pairproject.foodmap.repository.CategoryMapper;
 
 import java.net.URLDecoder;
@@ -39,28 +40,30 @@ public class CategoryService {
                 .filter(findName -> findName.equals(name))
                 .findAny().isEmpty(); //중복이 아니면 true
         if (!isDuplicateCheck) { //중복이면 예외 발생
-            throw new RuntimeException("이미 중복되는 이름이 있습니다.");
+            throw new DuplicateUserException("이미 중복되는 이름이 있습니다.");
         }
     }
 
     public String decodeName(String name) {
         String json = URLDecoder.decode(name, StandardCharsets.UTF_8);
-        return getTestByJson(json);
+        return getNameByJson(json);
     }
 
-    private static String getTestByJson(String json) {
+    public String getNameByJson(String json) {
         ObjectMapper mapper = new ObjectMapper();
+        JsonNode categoryIdNode = null;
         try {
             JsonNode rootNode = mapper.readTree(json);
-            JsonNode categoryIdNode = rootNode.get("categoryId");
+            categoryIdNode = rootNode.get("categoryId");
 
-            if (categoryIdNode != null) {
-                return categoryIdNode.asText();
-            } else {
-                throw new RuntimeException("categoryId 키를 찾을 수 없습니다.");
-            }
         } catch (Exception e) {
-            return "오류가 발생했습니다.";
+            throw new JsonParsingException("JSON 파싱 오류");
+        }
+
+        if (categoryIdNode != null) {
+            return categoryIdNode.asText();
+        } else {
+            throw new JsonParsingException("categoryId 키 없음");
         }
     }
 
